bc5b13316d25676dcd8a6c1f7b89613655c560a8
Skeeb32 <skeeb32@gmail.com>
2020-12-09 09:04:19 -0800

    Associations Pt. 2

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /friends
   # GET /friends.json
@@ -14,7 +16,7 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -24,7 +26,7 @@ class FriendsController < ApplicationController
   # POST /friends
   # POST /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -60,6 +62,13 @@ class FriendsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friend_path, notice: "Not Authorized To Edit This Friend" if @friend.nil?
+end
+
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
